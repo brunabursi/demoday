b87e03129a7241f8e5b89eb0a8cc901173fcb9ed
brunabursi <bruna.bursi@outlook.com>
2018-09-04 13:51:56 -0300

    adc arquivo barra

---
+++ static/barra.js
@@ -0,0 +1,17 @@
+/*fução para mover a barra lateral*/
+
+let botao = document.querySelector("#hamburger");
+let lateral = document.querySelector("aside");
+let progresso = document.querySelector('#progresso');
+
+botao.onclick = function(){
+    lateral.classList.toggle('mostrando');
+    for(item of lateral.children){
+        if(item.id !== "hamburger"){
+            item.classList.toggle("invisivel");
+        }
+    }
+    for(item of botao.children){
+        item.classList.toggle('mostrando');
+    }
+}
