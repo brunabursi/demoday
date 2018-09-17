d9ad16403911fa6b2d70f841b9d92428d7b3cc44
rodrigo.cano <rodrigo.cano@RodrigoCano.local>
2018-09-17 17:59:41 -0300

    ajuste do menu para o mobile com adição da animação e posicionamento dos elementos na home

--- static/barra.js
+++ static/barra.js
@@ -5,13 +5,13 @@ let lateral = document.querySelector("aside");
 let progresso = document.querySelector('#progresso');
 
 botao.onclick = function(){
-    lateral.classList.toggle('mostrando');
+    lateral.classList.toggle("mostrando");
     for(item of lateral.children){
         if(item.id !== "hamburger"){
             item.classList.toggle("invisivel");
         }
     }
     for(item of botao.children){
-        item.classList.toggle('mostrando');
+        item.classList.toggle("mostrando");
     }
 }
