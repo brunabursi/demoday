dc064b90f69abc99f6593985e6b2aa01f677c8f2
rodrigo.cano <rodrigo.cano@RodrigoCano.local>
2018-09-20 16:18:44 -0300

    Ajustes no panel e footer

--- static/script.js
+++ static/script.js
@@ -1,11 +1,14 @@
 let info = document.querySelector('#panel');
 let devs = document.querySelector('.devs');
+let footer = document.querySelector('footer');
 
 function showInfo() {
     if (info.style.display === "none") {
         info.style.display = "block";
+        footer.style.position = "inherit";
     } else {
         info.style.display = "none";
         devs.style.position = "relative";
+        footer.style.position = "absolute";
     }
 }
